b056597f50a8cd134242bdc5750757e74246a704
Chris Chicas <christianchicas7@gmail.com>
2022-07-09 22:55:26 -0400

    continued logic for main feed and article details page

--- src/components/articles/ArticleDetails.js
+++ src/components/articles/ArticleDetails.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function ArticleDetails(){
-    let params = useParams()
-    // const response = await fetch(url/articles/params.articleId)
-    // const data = await response.json()
-    //return article data
+    const [article, setArticle] = useState([])
+    let {articleId} = useParams()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/articles/${articleId}`)
+            const data = await response.json()
+            setArticle(data)
+        }
+        fetchData()
+    }, [articleId])
+
     return(
         <main>
-            <h1>Article Details For Article {params.articleId}</h1>    
+            <h1>Article Details For Article "{article.title}"</h1>
+            <p>{article.content}</p>  
         </main>       
     )
 }
+
+//add comments section, author edit/delete buttons
