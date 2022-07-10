a103b6d3536e4028862994aedf49b47cf82bf633
OLATUNDE <olatundequadril@gmail.com>
2022-07-09 21:43:47 -0400

    login & Signup

--- src/components/users/SignUpForm.jsx
+++ src/components/users/SignUpForm.jsx
@@ -20,39 +20,74 @@ const SignUpForm = () => {
 	}
 
   return (
-    <main>
-			<h1>Sign Up</h1>
-			<form onSubmit={verification}>
-				{error ? <div className="alert alert-danger alert-dismissible" role="alert">
-					Username already exists.
-					<button type="button" className="close" onClick={() => {setError(false)}} data-dismiss="alert" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-					</div> 
-					: null}
-				<div className="row">
-					<div className="col-sm-6 form-group">
-						<label htmlFor="firstName">First Name</label>
-						<input value={name.firstName} id="firstName" className="form-control" placeholder="Enter First Name" onChange={e => {setName({...name, firstName: e.target.value})}} required/>
-					</div>
-					<div className="col-sm-6 form-group">
-						<label htmlFor="lastName">Last Name</label>
-						<input value={name.lastName} className="form-control" id="lastName" placeholder="Enter Last Name" onChange={e => {setName({...name, lastName: e.target.value})}} required />
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-sm-6 form-group">
-						<label htmlFor="username">Username</label>
-						<input type="text" value={username} className="form-control" id="username" placeholder="Enter Desired Username" onChange={e => {setUserName(e.target.value)}} required/>
-					</div>
-					<div className="col-sm-6 form-group">
-						<label htmlFor="password">Password</label>
-						<input type="password" value={password} className="form-control" id="password" name="password" placeholder="Enter Desired Password" onChange={e => {setPassword(e.target.value)}} required/>
-					</div>
-				</div>
-				<input className="btn btn-primary" type="submit" value="Sign Up" />
-			</form>
-		</main>
+
+		<div className='container'>
+		<h3 className='my-4 mx-16 text-3xl font-bold'>Sign Up</h3>  
+		<form className="mx-10" onSubmit={verification}>
+				<>{error ? <div className="alert alert-danger alert-dismissible" role="alert">
+									Username already exists.
+									<button type="button" className="close" onClick={() => {setError(false)}} data-dismiss="alert" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+									</div> 
+									: null}</>
+			<ul className="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+				<li className="py-2 pl-2 border border-slate-300">
+					<label className="text-blue" htmlFor="firstname">Firstname: </label>
+					<input
+					className="border-2 rounded-lg border-stone-300"
+					value={name.firstName}
+					onChange={e => {setName({...name, firstName: e.target.value})}}
+					type="text"
+					id="firstname"
+					name="firstname"
+					required
+					/>
+				</li>
+				<li className="py-2 pl-2 border border-slate-300">
+					<label className="text-blue" htmlFor="lastname">Lastname: </label>
+					<input
+					className="border-2 rounded-lg border-stone-300"
+					value={name.lastName}
+					onChange={e => {setName({...name, lastName: e.target.value})}}
+					id="lastname"
+					name="lastname"
+					required
+					/>
+									
+				</li>
+				<li className="py-2 pl-2 border border-slate-300">
+					<label className="text-blue" htmlFor="username">Username: </label>
+					<input
+					className="border-2 rounded-lg border-stone-300 w-9/12"
+					value={username}
+					onChange={e => {setUserName(e.target.value)}}
+					type="text"
+					id="username"
+					name="username"
+					required
+					/>
+				</li>
+				<li className="py-2 pl-2 border border-slate-300">
+					<label className="text-blue" htmlFor="password">Password: </label>
+					<input
+					className="border-2 rounded-lg border-stone-300 w-9/12"
+					value={password}
+					onChange={e => {setPassword(e.target.value)}}
+					type="text"
+					id="password"
+					name="password"
+					required
+					/>
+				</li>
+			</ul>
+			<button 
+					className="hover:bg-indigo-500 my-4 inline-flex items-center justify-center p-2 bg-indigo-400 rounded-md shadow-lg"
+					type="submit">
+					Submit
+			</button>
+		</form>
+		</div>
   )
 }
 
